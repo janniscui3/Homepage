bf832ceca0106c9a4de4144ffd8fcb44f42b6a74
janniscui3 <33724337+janniscui3@users.noreply.github.com>
2017-12-05 10:21:31 +0100

    Add files via upload

---
+++ head.js
@@ -0,0 +1,19 @@
+//UserName = prompt("Bitte gib deinen Namen an.", "Name"); // prompt for user
+//document.write("<h1><center> Hallo " + UserName + "<\center><\/h1>"); 
+
+function ClickFunction(){
+	document.getElementById("panel").style.display = "block";
+}
+function clickfunction(){
+	document.getElementById("panel").style.display = "hidden";
+}
+			//function ausgabe(vorname) { 
+			//	alert ('JavaScript-Tutorial für ' + vorname  + ' - jetzt JavaScript lernen');
+			//	return(0) shows 0 if everything went fine
+			//	}
+			//var name = 'Sonja';
+			//ausgabe(name);
+			//console.log(name) Gibt Firebugs console den namen
+			//var para = document.getElementsByClassName("rot")
+			//var inhalt = document.getElementsByTagName('p'); nimmt jedes <p>
+			//var inhalt = document.getElementsByTagName('p')[1].innerHTML; nimmt nun das zweite <p>
